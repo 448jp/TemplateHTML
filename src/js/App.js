223e35dd27fdc85486d20e4a08a2cf9d386ff9f6
OKI Yoshiya <oki@ceroan.jp>
2017-08-07 16:51:19 +0900

    Update initial code

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,3 @@
-'use strict';
+$(document).ready(() => {
 
-setTimeout(function () {
-	var hoge = arguments.length <= 0 || arguments[0] === undefined ? "ほげ" : arguments[0];
-
-	console.log(hoge);
-}, 500);
+});
